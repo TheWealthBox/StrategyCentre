2550470be9d25991d8d58c852a9a73e4ea576d75
TheWealthBox <scintificinvestor@gmail.com>
2025-05-31 05:29:03 +0800

    Fix: Codepen infographic display issues

--- src/InfographicPage.js
+++ src/InfographicPage.js
@@ -1,26 +1,23 @@
-// src/InfographicPage.js
 import React from 'react';
-import './InfographicPage.css'; // Make sure you also create this CSS file
+import './InfographicPage.css'; // Make sure this import is there
 
 const InfographicPage = () => {
   return (
     <div className="infographic-container">
-      <h2>Infographic: The Power of Diversification</h2>
+      <h1>Infographic: The Power of Diversification</h1>
       <p>Explore this interactive infographic on investment strategies.</p>
       <iframe
-        height="2000"  // <-- SIGNIFICANTLY INCREASED HEIGHT
-        style={{ width: '100%' }} // Use JavaScript object for inline styles
-        scrolling="no" // 'no' means no scrollbars for the iframe itself
-        frameBorder="no" // Changed from 'frameborder' to 'frameBorder' for JSX
-        loading="lazy"
-        allowTransparency="true" // Changed from 'allowtransparency' to 'allowTransparency' for JSX
-        allowFullScreen={true} // Changed from 'allowfullscreen' to 'allowFullScreen' for JSX. Value is a boolean.
-        src="https://codepen.io/Scintific-Investor/embed/preview/GgJoqRL?default-tab=result" // This is the embed URL from Codepen
-        title="Infographic Article: The Power of Diversification"
+        id="quant-strategy-mixer"
+        src="https://codepen.io/Scientific-Investor/embed/preview/GRJqoML?default-tab=result"
+        // Ensure width is 100% and height is sufficient
+        width="100%"
+        height="800px" // Increased height to prevent scroll and "Run Pen" block
+        frameBorder="0"
+        allowTransparency="true"
+        allowFullScreen="true"
+        title="Quant Strategy Mixer: Smarter Investing Infographic"
       >
-        {/* The fallback content for browsers that don't support iframes */}
-        See the Pen <a href="https://codepen.io/Scintific-Investor/pen/GgJoqRL">Infographic Article: The Power of Diversification</a> by Scintific-Investor (<a href="https://codepen.io/Scintific-Investor">@Scintific-Investor</a>)
-        on <a href="https://codepen.io">CodePen</a>.
+        See the Pen <a href="https://codepen.io/Scientific-Investor/pen/GRJqoML">Quant Strategy Mixer: Smarter Investing Infographic</a> by Scientific Investor (<a href="https://codepen.io/Scientific-Investor">@Scientific-Investor</a>) on <a href="https://codepen.io">CodePen</a>.
       </iframe>
     </div>
   );
